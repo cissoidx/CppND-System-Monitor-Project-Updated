934b652264093d7072c9e291320733a8dff1f421
xu dong <xu.dong.gx@renesas.com>
2024-08-12 17:05:28 +0800

    update format

--- src/format.cpp
+++ src/format.cpp
@@ -14,14 +14,15 @@ string Format::ElapsedTime(long seconds) {
     int mm;
     int ss;
     string min_str;
+    string sec_str;
     string format_time;
     hh = seconds / 3600;
     mm = (seconds - hh*3600) / 60;
     ss = seconds - hh*3600 - mm*60;
-    if (mm<10)
-        {min_str = "0"+to_string(mm);}
-    else 
-        {min_str = to_string(mm);}
+    min_str = to_string(mm);
+    min_str.insert(0, 2-min_str.length(), '0');
+    sec_str = to_string(mm);
+    sec_str.insert(0, 2-sec_str.length(), '0');
     format_time = to_string(hh)+":"+min_str+":"+to_string(ss);
     return format_time;
 }
